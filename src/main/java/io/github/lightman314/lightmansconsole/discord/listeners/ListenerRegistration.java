b0b6757cf270a3447e53fcf34d735ade9e815d69
Jonathan Eshom <lightman314@gmail.com>
2022-03-20 21:51:09 -0400

    Removed currency bot for transfer.

--- src/main/java/io/github/lightman314/lightmansconsole/discord/listeners/ListenerRegistration.java
+++ src/main/java/io/github/lightman314/lightmansconsole/discord/listeners/ListenerRegistration.java
@@ -1,11 +1,9 @@
 package io.github.lightman314.lightmansconsole.discord.listeners;
 
 import io.github.lightman314.lightmansconsole.Config;
-import io.github.lightman314.lightmansconsole.LightmansDiscordIntegration;
 import io.github.lightman314.lightmansconsole.discord.listeners.account.AccountMessageListener;
 import io.github.lightman314.lightmansconsole.discord.listeners.chat.ChatMessageListener;
 import io.github.lightman314.lightmansconsole.discord.listeners.console.ConsoleMessageListener;
-import io.github.lightman314.lightmansconsole.discord.listeners.currency.CurrencyListener;
 import io.github.lightman314.lightmansconsole.events.JDAInitializedEvent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -29,12 +27,6 @@ public class ListenerRegistration {
 		//Add the account linking listeners
     	event.getProxy().addListener(new AccountMessageListener());
     	//event.getProxy().addListener(new AccountAdminListener(Config.SERVER.accountAdminChannel.get()));
-    	if(LightmansDiscordIntegration.isLightmansCurrencyLoaded())
-    	{
-    		CurrencyListener cl = new CurrencyListener(Config.SERVER.currencyChannel::get);
-    		MinecraftForge.EVENT_BUS.register(cl);
-    		event.getProxy().addListener(cl);
-    	}
 	}
 	
 }
